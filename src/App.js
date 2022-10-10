a98687dfb3f08f2fab9c0777422e3587b5f7038a
ziayamin <ziayamin8@gmail.com>
2022-10-10 15:42:53 +0430

    set up tailwind css

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
+import { Component } from "react";
+import "./App.css";
 
-import { Component } from 'react';
-import './App.css';
-
-class  App extends Component {
-
-constructor(){
-  super();
-  this.state={
-    monsters:[]
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
   }
-}
-componentDidMount(){
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response =>response.json())
-  .then(users=>this.setState(()=>{
-    return {monsters:users}
-  },
-  ()=>{
-    console.log(this.state)
-  }))
-}
-  render (){
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  render() {
     return (
       <div className="App">
-       {this.state.monsters.map((monster)=>{
-        return <h1 key={monster.id}>{monster.name} </h1>
-       })}
+        <div class="my-10">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="block mx-auto py-2 rounded-full border-gray-700 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            placeholder="Search monsters"
+          />
+        </div>
+        {this.state.monsters.map((monster) => {
+          return (
+            <h1 className="text-xl font-semibold" key={monster.id}>
+              {monster.name}{" "}
+            </h1>
+          );
+        })}
       </div>
     );
   }
