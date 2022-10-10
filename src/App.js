8c6dbd5d914879272b59703314bdda2eb46cfd69
ziayamin <ziayamin8@gmail.com>
2022-10-10 09:31:57 +0430

    exercise state and map and array object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+
 import { Component } from 'react';
 import './App.css';
 
 class  App extends Component {
+
+constructor(){
+  super();
+  this.state={
+    monsters:[
+      {
+        name:'Linda'
+      },
+      {
+        name:'Frank'
+      },
+      {
+        name:'Jack'
+      },
+      {
+        name:'Ali'
+      }
+    ]
+  }
+
+}
   render (){
     return (
       <div className="App">
-       
+       {this.state.monsters.map((monster,i)=>{
+        return <h1 key={i}>{monster.name}</h1>
+       })}
       </div>
     );
   }
