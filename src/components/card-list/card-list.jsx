3af4fa3c05c75bb54ce586aff995a7e0699ea912
ziayamin <ziayamin8@gmail.com>
2022-11-15 21:05:12 +0430

    create a component for card list

---
+++ src/components/card-list/card-list.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CardList extends Component {
+    render() {
+        return (
+            <div>
+                Hey! what is your name?
+            </div>
+        );
+    }
+}
+
+export default CardList;
