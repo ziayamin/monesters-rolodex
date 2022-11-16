a60a58b3eddfc064fa185965976620902ea920c7
ziayamin <ziayamin8@gmail.com>
2022-11-16 23:11:21 +0430

    change style

--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -8,7 +8,7 @@ class SearchBox extends Component{
               type="search"
               name={this.props.name}
               id={this.props.id}
-              className="block px-4 py-2 mx-auto border-gray-700 rounded-full shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              className="block px-4 py-2 w-[20%] mx-auto border-gray-700 rounded-full shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
               placeholder={this.props.placeholder}
               onChange={this.props.onChangeHandler}
             />
